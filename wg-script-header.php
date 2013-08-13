5568f8873764eb9ffabad02387fc0b6715a90bfd
Marco Lopez <mlopez@virtualsip.net>
2013-08-13 17:36:55 -0500

    agregando "base" para ruta de la web

--- wg-script-header.php
+++ wg-script-header.php
@@ -1,3 +1,5 @@
+<base href="<?php echo $web; ?>"></base>
+
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
 <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
