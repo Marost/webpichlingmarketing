12a5eb90d18f163d12e1469489c9f77760d82994
Marco Lopez <mlopez@virtualsip.net>
2013-08-14 18:07:00 -0500

    pagina de eventos

---
+++ eventos.php
@@ -0,0 +1,221 @@
+<?php
+//VARIABLES
+$sc_home=false;
+$sc_slider=false;
+?>
+<!DOCTYPE HTML>
+<html lang="es-ES">
+<head>
+	<meta charset="UTF-8">
+	<title>Eventos | Pichling Sports Marketing</title>
+
+	<?php require_once("wg-script-header.php"); ?>
+
+</head>
+<body class="l-body">
+
+<div class="l-background"></div>
+
+<!-- CANVAS -->
+<div class="l-canvas type_boxed col_cont">
+	<div class="l-canvas-h">
+
+		<!-- HEADER -->
+		<?php require_once("wg-header.php"); ?>
+		<!-- /HEADER -->
+
+		<!-- MAIN -->
+		<div class="l-main">
+			<div class="l-main-h">
+
+				<div class="l-submain for_pagehead">
+					<div class="l-submain-h g-html i-cf">
+						<div class="w-pagehead">
+							<h1>Eventos Producidos</h1>
+							<p></p>
+							<!-- breadcrums -->
+							<div class="g-breadcrumbs">
+								<a href="/" class="g-breadcrumbs-item">Inicio</a>
+								<span class="g-breadcrumbs-separator">&raquo;</span>
+								<span class="g-breadcrumbs-item">Eventos</span>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+				<div class="l-submain">
+					<div class="l-submain-h g-html">
+
+						<div class="w-portfolio columns_4 type_sortable">
+							<div class="w-portfolio-h">
+								<div class="w-portfolio-list">
+								<div class="w-portfolio-list-h">
+								
+									<div class="w-portfolio-item order_1 naming webdesign">
+									<div class="w-portfolio-item-h">
+										<a class="w-portfolio-item-anchor" href="project.html">
+											<div class="w-portfolio-item-image">
+												<img src="imagenes/eventos/01.jpg" alt=""/>
+												<div class="w-portfolio-item-meta">
+													<h2 class="w-portfolio-item-title">Presentación de Equipo Profesional Universitario de Deportes 2012</h2>
+												</div>
+											</div>
+										</a>
+									</div>
+									</div>
+
+									<div class="w-portfolio-item order_2 naming branding">
+									<div class="w-portfolio-item-h">
+										<a class="w-portfolio-item-anchor" href="project.html">
+											<div class="w-portfolio-item-image">
+												<img src="imagenes/eventos/02.jpg" alt=""/>
+												<div class="w-portfolio-item-meta">
+													<h2 class="w-portfolio-item-title">Torneo Inter Empresas Mineras “Copa Manuel Plenge”</h2>
+												</div>
+											</div>
+										</a>
+									</div>
+									</div>
+
+									<div class="w-portfolio-item order_3 webdesign illustration">
+									<div class="w-portfolio-item-h">
+										<a class="w-portfolio-item-anchor" href="project.html">
+											<div class="w-portfolio-item-image">
+												<img src="imagenes/eventos/03.jpg" alt=""/>
+												<div class="w-portfolio-item-meta">
+													<h2 class="w-portfolio-item-title">Torneo Interno de Voley Mixto</h2>
+												</div>
+											</div>
+										</a>
+									</div>
+									</div>
+
+									<div class="w-portfolio-item order_4 photography naming">
+									<div class="w-portfolio-item-h">
+										<a class="w-portfolio-item-anchor" href="project.html">
+											<div class="w-portfolio-item-image">
+												<img src="imagenes/eventos/04.jpg" alt=""/>
+												<div class="w-portfolio-item-meta">
+													<h2 class="w-portfolio-item-title">1ra Triatlón de Lima</h2>
+												</div>
+											</div>
+										</a>
+									</div>
+									</div>
+
+									<div class="w-portfolio-item order_5 webdesign">
+									<div class="w-portfolio-item-h">
+										<a class="w-portfolio-item-anchor" href="project.html">
+											<div class="w-portfolio-item-image">
+												<img src="imagenes/eventos/05.jpg" alt=""/>
+												<div class="w-portfolio-item-meta">
+													<h2 class="w-portfolio-item-title">Olimpiadas Volcan</h2>
+												</div>
+											</div>
+										</a>
+									</div>
+									</div>
+
+									<div class="w-portfolio-item order_6 photography illustration">
+									<div class="w-portfolio-item-h">
+										<a class="w-portfolio-item-anchor" href="project.html">
+											<div class="w-portfolio-item-image">
+												<img src="imagenes/eventos/06.jpg" alt=""/>
+												<div class="w-portfolio-item-meta">
+													<h2 class="w-portfolio-item-title">XXI Sudamericano Juvenil de Vóley Femenino Copa Unique 2012</h2>
+												</div>
+											</div>
+										</a>
+									</div>
+									</div>
+
+									<div class="w-portfolio-item order_7 naming branding">
+									<div class="w-portfolio-item-h">
+										<a class="w-portfolio-item-anchor" href="project.html">
+											<div class="w-portfolio-item-image">
+												<img src="imagenes/eventos/07.jpg" alt=""/>
+												<div class="w-portfolio-item-meta">
+													<h2 class="w-portfolio-item-title">XVIII Campeonato Sudamericano de Menores de Vóley Copa UAP Perú 2012</h2>
+												</div>
+											</div>
+										</a>
+									</div>
+									</div>
+
+									<div class="w-portfolio-item order_8 illustration branding">
+									<div class="w-portfolio-item-h">
+										<a class="w-portfolio-item-anchor" href="project.html">
+											<div class="w-portfolio-item-image">
+												<img src="imagenes/eventos/08.jpg" alt=""/>
+												<div class="w-portfolio-item-meta">
+													<h2 class="w-portfolio-item-title">Cuadrangular Internacional de Vóley - Copa Unique 2013</h2>
+												</div>
+											</div>
+										</a>
+									</div>
+									</div>
+
+									<div class="w-portfolio-item order_9 photography illustration">
+									<div class="w-portfolio-item-h">
+										<a class="w-portfolio-item-anchor" href="project.html">
+											<div class="w-portfolio-item-image">
+												<img src="imagenes/eventos/09.jpg" alt=""/>
+												<div class="w-portfolio-item-meta">
+													<h2 class="w-portfolio-item-title">Partido Amistoso Internacional - Alianza Lima (PE) vs Manta FC (EC)</h2>
+												</div>
+											</div>
+										</a>
+									</div>
+									</div>
+									
+									<div class="w-portfolio-item order_10 webdesign illustration">
+									<div class="w-portfolio-item-h">
+										<a class="w-portfolio-item-anchor" href="project.html">
+											<div class="w-portfolio-item-image">
+												<img src="imagenes/eventos/10.jpg" alt=""/>
+												<div class="w-portfolio-item-meta">
+													<h2 class="w-portfolio-item-title">Corre Antamina 10K</h2>
+												</div>
+											</div>
+										</a>
+									</div>
+									</div>
+
+									<div class="w-portfolio-item order_11 webdesign">
+									<div class="w-portfolio-item-h">
+										<a class="w-portfolio-item-anchor" href="project.html">
+											<div class="w-portfolio-item-image">
+												<img src="imagenes/eventos/11.jpg" alt=""/>
+												<div class="w-portfolio-item-meta">
+													<h2 class="w-portfolio-item-title">Torneo Inter Empresa Mineras “Copa Alejandro Fang”</h2>
+												</div>
+											</div>
+										</a>
+									</div>
+									</div>
+
+								</div>
+								</div>
+								
+							</div>
+						</div>
+
+					</div>
+				</div>
+				
+			</div>
+		</div>
+		<!-- /MAIN -->
+
+	</div>
+</div>
+<!-- /CANVAS -->
+
+<!-- FOOTER -->
+<?php require_once("wg-footer.php"); ?>
+<!-- /FOOTER -->
+
+<?php require_once("wg-script-footer.php"); ?>
+
+</body>
+</html>
