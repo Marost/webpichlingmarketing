5fa219b62db7a99399129d99b79f4c240d74648a
Marco Lopez <mlopez@virtualsip.net>
2013-09-04 15:49:33 -0500

    galeria de fotos y galeria seleccionada en home

---
+++ galeria-seleccionada.php
@@ -0,0 +1,161 @@
+<!DOCTYPE HTML>
+<html lang="es-ES">
+<head>
+	<meta charset="UTF-8">
+	<title>Media</title>
+
+	<?php require_once("wg-script-header.php"); ?>
+
+</head>
+<body class="l-body">
+
+<div class="l-background"></div>
+
+<!-- CANVAS -->
+<div class="l-canvas type_boxed col_cont">
+	<div class="l-canvas-h">
+
+		<!-- HEADER -->
+		<?php require_once("wg-header.php"); ?>
+		<!-- /HEADER -->
+
+		<!-- MAIN -->
+		<div class="l-main">
+			<div class="l-main-h">
+
+				<div class="l-submain for_pagehead">
+					<div class="l-submain-h g-html i-cf">
+						<div class="w-pagehead">
+							<h1>Torneo MIQ Logistics 2013</h1>
+							<p></p>
+							<!-- breadcrums -->
+							<div class="g-breadcrumbs">
+								<a href="/" class="g-breadcrumbs-item">Inicio</a>
+								<span class="g-breadcrumbs-separator">&raquo;</span>
+								<span class="g-breadcrumbs-item">Galería de Fotos</span>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+				<div class="l-submain">
+					<div class="l-submain-h g-html">
+
+						<div class="w-gallery layout_tile size_s">
+							<div class="w-gallery-h">
+								<div class="w-gallery-tnails">
+									<div class="w-gallery-tnails-h">
+									
+										<a class="w-gallery-tnail order_1" href="imagenes/placeholder/1200x800.gif">
+											<span class="w-gallery-tnail-h">
+												<img class="w-gallery-tnail-img" src="imagenes/placeholder/220x220.gif" alt="Tit 1"/>
+												<span class="w-gallery-tnail-title"><i class="icon-search"></i></span>
+											</span>
+										</a>
+
+										<a class="w-gallery-tnail order_2" href="imagenes/placeholder/600x900.gif">
+											<span class="w-gallery-tnail-h">
+												<img class="w-gallery-tnail-img" src="imagenes/placeholder/220x220.gif" alt="photo"/>
+												<span class="w-gallery-tnail-title"><i class="icon-search"></i></span>
+											</span>
+										</a>
+
+										<a class="w-gallery-tnail order_3" href="imagenes/placeholder/1200x800.gif">
+											<span class="w-gallery-tnail-h">
+												<img class="w-gallery-tnail-img" src="imagenes/placeholder/220x220.gif" alt="photo"/>
+												<span class="w-gallery-tnail-title"><i class="icon-search"></i></span>
+											</span>
+										</a>
+
+										<a class="w-gallery-tnail order_4" href="imagenes/placeholder/600x900.gif">
+											<span class="w-gallery-tnail-h">
+												<img class="w-gallery-tnail-img" src="imagenes/placeholder/220x220.gif" alt="photo"/>
+												<span class="w-gallery-tnail-title"><i class="icon-search"></i></span>
+											</span>
+										</a>
+
+										<a class="w-gallery-tnail order_5" href="imagenes/placeholder/1200x800.gif">
+											<span class="w-gallery-tnail-h">
+												<img class="w-gallery-tnail-img" src="imagenes/placeholder/220x220.gif" alt="photo"/>
+												<span class="w-gallery-tnail-title"><i class="icon-search"></i></span>
+											</span>
+										</a>
+
+										<a class="w-gallery-tnail order_6" href="imagenes/placeholder/600x900.gif">
+											<span class="w-gallery-tnail-h">
+												<img class="w-gallery-tnail-img" src="imagenes/placeholder/220x220.gif" alt="photo"/>
+												<span class="w-gallery-tnail-title"><i class="icon-search"></i></span>
+											</span>
+										</a>
+
+										<a class="w-gallery-tnail order_7" href="imagenes/placeholder/1200x800.gif">
+											<span class="w-gallery-tnail-h">
+												<img class="w-gallery-tnail-img" src="imagenes/placeholder/220x220.gif" alt="photo"/>
+												<span class="w-gallery-tnail-title"><i class="icon-search"></i></span>
+											</span>
+										</a>
+
+										<a class="w-gallery-tnail order_8" href="imagenes/placeholder/600x900.gif">
+											<span class="w-gallery-tnail-h">
+												<img class="w-gallery-tnail-img" src="imagenes/placeholder/220x220.gif" alt="photo"/>
+												<span class="w-gallery-tnail-title"><i class="icon-search"></i></span>
+											</span>
+										</a>
+
+										<a class="w-gallery-tnail order_9" href="imagenes/placeholder/1200x800.gif">
+											<span class="w-gallery-tnail-h">
+												<img class="w-gallery-tnail-img" src="imagenes/placeholder/220x220.gif" alt="photo"/>
+												<span class="w-gallery-tnail-title"><i class="icon-search"></i></span>
+											</span>
+										</a>
+
+										<a class="w-gallery-tnail order_10" href="imagenes/placeholder/600x900.gif">
+											<span class="w-gallery-tnail-h">
+												<img class="w-gallery-tnail-img" src="imagenes/placeholder/220x220.gif" alt="photo"/>
+												<span class="w-gallery-tnail-title"><i class="icon-search"></i></span>
+											</span>
+										</a>
+
+										<a class="w-gallery-tnail order_11" href="imagenes/placeholder/600x900.gif">
+											<span class="w-gallery-tnail-h">
+												<img class="w-gallery-tnail-img" src="imagenes/placeholder/220x220.gif" alt="photo"/>
+												<span class="w-gallery-tnail-title"><i class="icon-search"></i></span>
+											</span>
+										</a>
+
+										<a class="w-gallery-tnail order_12" href="imagenes/placeholder/600x900.gif">
+											<span class="w-gallery-tnail-h">
+												<img class="w-gallery-tnail-img" src="imagenes/placeholder/220x220.gif" alt="photo"/>
+												<span class="w-gallery-tnail-title"><i class="icon-search"></i></span>
+											</span>
+										</a>
+
+									</div>
+								</div>
+							</div>
+						</div>
+					
+						<div class="hr hr_invisible">
+							<span class="hr-h">
+								<span class="hr-hh"></span>
+							</span>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- /MAIN -->
+
+	</div>
+</div>
+<!-- /CANVAS -->
+
+<!-- FOOTER -->
+<?php require_once("wg-footer.php"); ?>
+<!-- /FOOTER -->
+
+<?php require_once("wg-script-footer.php"); ?>
+
+</body>
+</html>
---
+++ galeria.php
@@ -0,0 +1,115 @@
+<?php
+include("panel@pmarketing/conexion/conexion.php");
+include("panel@pmarketing/conexion/funciones.php");
+
+//VARIABLES
+$sc_home=false;
+$sc_slider=false;
+
+//GALERIA DE F_galeria
+$rst_galeria=mysql_query("SELECT * FROM pmkt_galeria ORDER BY fecha_publicacion DESC, id DESC;", $conexion);
+
+?>
+<!DOCTYPE HTML>
+<html lang="es-ES">
+<head>
+	<meta charset="UTF-8">
+	<title>Galería de Fotos | Pichling Sports Marketing</title>
+
+	<?php require_once("wg-script-header.php"); ?>
+
+</head>
+<body class="l-body">
+
+<div class="l-background"></div>
+
+<!-- CANVAS -->
+<div class="l-canvas type_boxed col_cont">
+	<div class="l-canvas-h">
+
+		<!-- HEADER -->
+		<?php require_once("wg-header.php"); ?>
+		<!-- /HEADER -->
+
+		<!-- MAIN -->
+		<div class="l-main">
+			<div class="l-main-h">
+
+				<div class="l-submain for_pagehead">
+					<div class="l-submain-h g-html i-cf">
+						<div class="w-pagehead">
+							<h1>Galería de Fotos</h1>
+							<p></p>
+							<!-- breadcrums -->
+							<div class="g-breadcrumbs">
+								<a href="/" class="g-breadcrumbs-item">Inicio</a>
+								<span class="g-breadcrumbs-separator">&raquo;</span>
+								<span class="g-breadcrumbs-item">Galería de Fotos</span>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+				<div class="l-submain">
+					<div class="l-submain-h g-html">
+
+						<div class="w-portfolio columns_4 type_sortable">
+							<div class="w-portfolio-h">
+								<div class="w-portfolio-list">
+									<div class="w-portfolio-list-h">
+									
+										<?php while($fila_galeria=mysql_fetch_array($rst_galeria)){
+												$galeria_id=$fila_galeria["id"];
+												$galeria_url=$fila_galeria["url"];
+												$galeria_titulo=$fila_galeria["titulo"];
+
+												//SELECCIONAR IMAGEN DE GALERIA
+												$rst_slide=mysql_query("SELECT * FROM pmkt_galeria_slide WHERE noticia=$galeria_id AND id=0", $conexion);
+												$fila_slide=mysql_fetch_array($rst_slide);
+
+												$galeria_imagen=$fila_slide["imagen"];
+												$galeria_imagen_carpeta=$fila_slide["carpeta"];
+
+												//URLS
+												$galeria_UrlWeb=$web."galeria/".$galeria_id."-".$galeria_url;
+												$galeria_UrlImagen=$web."imagenes/galeria/".$galeria_imagen_carpeta."thumb/".$galeria_imagen;
+										?>
+										<div class="w-portfolio-item order_1 naming webdesign">
+											<div class="w-portfolio-item-h">
+												<a class="w-portfolio-item-anchor" href="<?php echo $galeria_UrlWeb; ?>">
+													<div class="w-portfolio-item-image">
+														<img src="<?php echo $galeria_UrlImagen; ?>" alt="<?php echo $galeria_titulo; ?>"/>
+														<div class="w-portfolio-item-meta">
+															<h2 class="w-portfolio-item-title"><?php echo $galeria_titulo; ?></h2>
+														</div>
+													</div>
+												</a>
+											</div>
+										</div>
+										<?php } ?>
+
+									</div>
+								</div>
+								
+							</div>
+						</div>
+
+					</div>
+				</div>
+				
+			</div>
+		</div>
+		<!-- /MAIN -->
+
+	</div>
+</div>
+<!-- /CANVAS -->
+
+<!-- FOOTER -->
+<?php require_once("wg-footer.php"); ?>
+<!-- /FOOTER -->
+
+<?php require_once("wg-script-footer.php"); ?>
+
+</body>
+</html>
