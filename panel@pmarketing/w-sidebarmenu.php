e6ead8ee687fdfe1bbcaee70337afa67cb4a2c6f
Marco Lopez <mlopez@virtualsip.net>
2013-11-18 16:57:06 -0500

    Administrador: Nuevas opciones en menu (Empresa, Clientes, Galeria de Fotos)

--- panel@pmarketing/w-sidebarmenu.php
+++ panel@pmarketing/w-sidebarmenu.php
@@ -6,7 +6,10 @@
     
     <!-- Sidebar subnav -->
     <ul class="subNav">
+        <li><a href="<?php echo $url_admin; ?>paginas/empresa/lista.php" title=""><span class="icos-frames"></span>Empresa</a></li>
+        <li><a href="<?php echo $url_admin; ?>paginas/clientes/lista.php" title=""><span class="icos-frames"></span>Relaciones comerciales</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/eventos/lista.php" title=""><span class="icos-frames"></span>Eventos</a></li>
+        <li><a href="<?php echo $url_admin; ?>paginas/galeria/lista.php" title=""><span class="icos-frames"></span>Galería de Fotos</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/noticias/lista.php" title=""><span class="icos-frames"></span>Noticias</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/noticias-tag/lista.php" title=""><span class="icos-frames"></span>Noticias - Etiquetas</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/noticias-seccion/lista.php" title=""><span class="icos-frames"></span>Noticias - Secciones</a></li>
