67058c468cc48494712dee0886f1909339824e41
Marco Lopez <mlopez@virtualsip.net>
2013-12-16 15:40:41 -0500

    Home: Script de Google Analytics y FlexSlider

--- wg-script-footer.php
+++ wg-script-footer.php
@@ -27,4 +27,36 @@
 <script src="js/jquery.flexslider.js"></script>
 <?php } ?>
 
+<?php if($sc_slider==true){ ?>
+<script src="js/jquery.carousello.js"></script>
+<script src="js/jquery.flexslider.js"></script>
+<script type="text/javascript">
+	jQuery(window).load(function() {
+		jQuery('.flexslider').flexslider({
+			controlNav: false,
+			smoothHeight: true,
+			animation: "slide",
+			directionNav: false,  
+			start: function() {
+				slider.removeClass('flex-loading');
+			}
+		});
+	});
+</script>
+<?php } ?>
+
 <script src="js/w-lang.js"></script>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-20229980-17']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
