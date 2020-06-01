d4ab3422cd14376fa8bf49e6ce71fdfea6034367
Ronal Daniel Maita Vasquez <daniel_ronald17@hotmail.com>
2020-05-31 22:40:35 -0500

    Actualizacion de Privacidad

--- privacidad.php
+++ privacidad.php
@@ -122,79 +122,16 @@ $sc_home=true;
                                                     	<p><strong>PSM SPORTS MARKETING S.A.C.</strong> le recuerda al usuario que el tratamiento de la información necesaria para la ejecución de la relación contractual que lo vincula con nosotros no requiere de consentimiento.</p>
                                                     </ol>
                                                     <h4>¿A QUIÉNES PODEMOS COMPARTIR LA INFORMACIÓN?</h4>
-                                                    <p><strong>PSM SPORTS MARKETING S.A.C.</strong> se compromete a no divulgar, ceder o compartir la información del usuario sin que éste haya prestado el debido consentimiento para ello. En ese sentido, cuando el usuario otorgue el consentimiento, autorizará a <strong>PSM SPORTS MARKETING S.A.C.</strong> para compartir su información en los siguientes casos:</p>
+                                                    <p><strong>PSM SPORTS MARKETING S.A.C.</strong> se compromete a no divulgar, ceder o compartir la información del usuario sin que éste haya prestado el debido consentimiento para ello. En ese sentido, cuando el usuario otorgue el consentimiento, autorizará a <strong>PSM SPORTS MARKETING S.A.C.</strong> para compartir su información en los siguientes casos si fuesen necesarios:</p>
+                                                    <ol>
+                                                    	<li>Empresas Vinculadas</li>
+                                                        <li>Cesión de Posición Contractual</li>
+                                                        <li>Proveedores de Web y Aplicaciones</li>
+                                                        <li>Fusión y Adquisición</li>
+                                                        <li>Autoridades Públicas</li>
+                                                    </ol>
                                                     <!--
 
-
-
-
-
-1. Empresas Vinculadas
-
-A fin de que las empresas que forman parte de nuestro Grupo puedan contactar y ofrecer al Usuario información comercial, publicitaria y/o promocional sobre sus servicios y/o productos, ya sea a través de los Sitios Web, Aplicaciones, Telefonía, Mensajería de Texto o a través de otros medios electrónicos o no electrónicos.
-
-La lista de las empresas vinculadas al Grupo El Comercio se encuentran en el siguiente link: www.grupoelcomercio.com.pe .
-
-2. Cesión de Posición Contractual
-
-Los Usuarios deberán autorizar expresamente la cesión de Posición Contractual y de su Información en favor de cualquier persona que: (i) el Grupo El Comercio le haya cedido su posición contractual como titular de los sitios web y aplicaciones o para prestar los bienes y servicios; (ii) que sea el nuevo responsable de los bancos de datos que contengan la Información del Usuario. Luego de producida la cesión, el Grupo El Comercio no tendrá ninguna responsabilidad con respecto de cualquier hecho que ocurra partir de la fecha de la cesión. El nuevo responsable del banco de datos asumirá todas y cada una de las obligaciones del Grupo El Comercio establecidas en la presente Política respecto al tratamiento, resguardo y conservación de la Información del Usuario.
-
-Asimismo, el Grupo El Comercio podrá compartir la información de los Usuarios, incluso, sin su consentimiento, en los siguientes casos:
-
-1. Proveedores de Web y Aplicaciones del Grupo El Comercio
-
-A fin de que utilicen dicha información para mejorar o facilitar la operatividad de los Sitios Web y las Aplicaciones del Grupo El Comercio, así como permitir la ejecución de los servicios contratados por el Usuario.
-
-2. Fusión y Adquisición
-
-La información que es recolectada de los Usuarios, incluidos los Datos Personales, son considerados como activos de las empresas del Grupo El Comercio. Si alguna empresa del Grupo El Comercio fuera adquirida como resultado de una fusión, escisión, reorganización simple u otra modalidad, incluida la venta de activos, la información será transferida al tercero adquirente como parte de los activos vendidos
-
-3. Autoridades Públicas
-
-A fin de cumplir con solicitudes o disposiciones emitidas por:
-
-Autoridades públicas en ejercicio de sus funciones y el ámbito de sus competencias
-Órdenes judiciales.
-Disposiciones legales
-INFORMACIÓN SOBRE LA UBICACIÓN DEL USUARIO
-
-En algunas aplicaciones, El Grupo El Comercio puede colectar y almacenar información sobre la localización de los Usuarios a través del GPS de sus dispositivos móviles, con el fin de brindar un servicio más personalizado de acuerdo a su ubicación. En el caso que el Usuario no desee que recolectemos información sobre su ubicación, puede desactivar dicha opción directamente de su dispositivo móvil.
-
-PLATAFORMAS DE REDES SOCIALES
-
-Grupo El Comercio incluye dentro de sus plataformas, accesos a diversos blogs, foros, entre otros. Dichos sitios web pueden contener, en algunos casos, links o hipervínculos a diferentes redes o plataformas sociales públicas.
-
-Cualquier información personal, transmitida por el Usuario voluntariamente, en línea, en foros o blogs públicos, entre otros, podrá ser vista por terceras personas sin restricción alguna. En tal sentido, el Grupo El Comercio no se responsabilizará por la información publicada directamente por el Usuario, en la medida que la misma se encuentra fuera de su ámbito de control.
-
-PAGOS DE SERVICIOS A TRAVÉS DE TERCEROS
-
-Grupo El Comercio puede, a través de diversas plataformas, ofrecer suscripciones, productos o diversos servicios a los Usuarios. De esta manera, cuando se realice alguna compra o adquisición de servicios ofrecidos por el Grupo El Comercio mediante servicios de pago operados por terceros (incluyendo, Apple Store, Google Play, entre otros similares), dichas empresas podrán recolectar la información del Usuario, incluyendo nombre, correo electrónico, número de tarjeta de crédito o débito, entre otros, con el fin de procesar la compra.
-
-TRATAMIENTO TRANSFRONTERIZO
-
-En el caso que el usuario no domicilie en la República del Perú, su información personal podrá ser transferida y almacenada en bancos de datos ubicadas en Estados Unidos e Irlanda. Por tal motivo, el usuario deberá tener en cuenta que, países fuera su territorio podrían no tener las mismas leyes y derechos sobre protección de datos personales que su jurisdicción. No obstante ello, el Grupo El Comercio ha adoptado las medidas necesarias para garantizar que existan mecanismos y garantías adecuados (incluida la regulación de la Unión Europea) para asegurar la transferencia internacional de su información a territorio norte americano e irlandés.
-
-NUESTRA POLÍTICA PARA MENORES DE EDAD
-
-Para algunos de los servicios del Grupo El Comercio se necesita cierta edad para poder ser utilizados por los usuarios. Se precisa que, la edad requerida dependerá de la jurisdicción de cada país de residencia de los usuarios.
-
-El Grupo el Comercio no recopila ni solicita información personal de forma intencionada ni dirige publicidad basado en intereses a personas menores de edad, ni permitimos que dichas personas utilicen nuestros servicios.
-
-Sin embargo, en el caso que el Grupo El Comercio decida, eventualmente, dirigir algún servicio a menores de edad, se solicitará el consentimiento del padre, tutor o representante, con el fin de cumplir con la normativa sobre protección de datos En el caso se detecte que el Grupo El Comercio ha recolectado, sin intención alguna, información de menores de edad, la misma será eliminada de nuestra base de datos lo más pronto posible.
-
-Finalmente, si un menor de edad accede a alguna página web o APP del Grupo El Comercio a través de cualquier medio, se presume que dichos accesos se han realizado con el debido consentimiento de padres, tutores o representantes legales
-
-DERECHOS DE ACCESO, RECTIFICACIÓN, CANCELACIÓN, OPOSICIÓN Y TRATAMIENTO OBJETIVO DE DATOS
-
-Grupo El Comercio pone en conocimiento del Usuario sus derechos de acceso, rectificación, oposición, cancelación y tratamiento objetivo de su información personal, así como los demás derechos concedidos por la normativa de protección de datos personales, los mismos que se podrán ejercer a través del portal de Ventanilla Arco del Grupo el Comercio, el cual se encuentra en el siguiente: www.grupoelcomercio.com.pe
-
-Sin perjuicio de lo anterior, Grupo El Comercio podrá conservar determinada Información del Usuario que solicita la baja, a fin de que sirva de prueba ante una eventual reclamación contra Grupo El Comercio por responsabilidades derivadas del tratamiento de dicha información. La duración de dicha conservación no podrá ser superior al plazo de prescripción legal de dichas responsabilidades.
-
-PORTABILIDAD DE DATOS
-
-Grupo El Comercio pone en conocimiento del Usuario que en caso tenga sus datos registrados en nuestro Banco de Datos, puede ejercer su derecho a la Portabilidad de Datos hacia otro responsable del tratamiento de su información. Con el fin de Portar sus Datos, el Usuario deberá solicitarlo a través del portal de Ventanilla Arco del Grupo el Comercio, el cual se encuentra en el siguiente enlace: www.grupoelcomercio.com.pe brindando los datos y el contacto del nuevo responsable del tratamiento de su información.
-
-En caso sea un ciudadano europeo y se encuentre protegido por el Reglamento General de Protección de Datos (RGPD) El Grupo El Comercio evaluará si su solicitud cumple con los requisitos correspondientes y, en un plazo de 7 días hábiles, procederá a transferir la información al nuevo responsable, deslindando cualquier responsabilidad sobre el tratamiento de los datos después de efectuarse la portabilidad.
 
 ¿CÓMO RESGUARDAMOS LA INFORMACIÓN DEL USUARIO?
 
